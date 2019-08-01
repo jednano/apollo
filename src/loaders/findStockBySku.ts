efc895187da5e7a3d5d6193a85a6f7b6dbab8995
jedmao <jedmao@outlook.com>
2019-08-01 11:54:42 -0500

    key

--- src/loaders/findStockBySku.ts
+++ src/loaders/findStockBySku.ts
@@ -6,14 +6,12 @@ import sql from '../context/sql'
 export default new DataLoader(findStockBySkus)
 
 async function findStockBySkus(skus: string[]) {
+	const key = 'only_x_left_in_stock'
 	const products = await sql`
-SELECT cpe.sku, cisi.qty only_x_left_in_stock
+SELECT cpe.sku, cisi.qty ${key}
 FROM catalog_product_entity cpe
 	LEFT JOIN cataloginventory_stock_item cisi
 		ON cisi.product_id = cpe.entity_id
 WHERE cpe.sku IN (${sql.join(skus)})`
-	return map(
-		sortBy(products, ({ sku }) => skus.indexOf(sku)),
-		'only_x_left_in_stock',
-	)
+	return map(sortBy(products, ({ sku }) => skus.indexOf(sku)), key)
 }
